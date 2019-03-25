dd63620935320aef903e5324bf191ada96c72c21
Alexlbt <lambertalexis2@gmail.com>
2019-03-25 19:06:34 +0100

    React-router + linter sass + refacto build process + start

--- apps/front/src/index.js
+++ apps/front/src/index.js
@@ -1,26 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Style/Main.css';
+import './Style/app.css';
 import App from './Component/App';
-import registerServiceWorker from './ServiceWorker';
-import { applyMiddleware, createStore } from 'redux'
-import { createEpicMiddleware } from 'redux-observable'
-import rootEpic from './Epic'
-import { default as mainReducer, debug } from './Redux/State'
 
-const epicMiddleware = createEpicMiddleware({
-  dependencies: {
-    fetchDog: () => fetch('https://dog.ceo/api/breeds/image/random'),
-  },
-});
-const middleware     = applyMiddleware(epicMiddleware);
-const reducer        = process.env.REACT_APP_DEBUG_STATE
-  ? debug(mainReducer)
-  : mainReducer;
-const store          = createStore(reducer, reducer(), middleware);
-
-epicMiddleware.run(rootEpic);
-
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
-
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
